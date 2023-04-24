addb653cf23702a92787da282b70dffee73691c4
Joshua Morony <joshua.morony@gmail.com>
2023-04-24 13:27:11 +0930

    feat(signal-with-error): prototype

--- packages/signal-with-error/src/lib/signal-with-error.ts
+++ packages/signal-with-error/src/lib/signal-with-error.ts
@@ -1,3 +1,21 @@
-export function signalWithError(): string {
-  return 'signal-with-error';
+import { Signal, computed } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { Observable, of } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+export function toSignalWithError<T>(obs$: Observable<T>): {
+  value: Signal<T | undefined>;
+  error: Signal<any>;
+} {
+  const source = toSignal(
+    obs$.pipe(
+      map((value) => ({ value, error: undefined })),
+      catchError((err) => of({ value: undefined, error: err }))
+    )
+  );
+
+  const value = computed(() => source()?.value);
+  const error = computed(() => source()?.error);
+
+  return { value, error };
 }
